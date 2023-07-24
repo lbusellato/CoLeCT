Fixed quaternion coordinate transformation

samples/kmp.py
@@ -25,7 +25,7 @@ force_ = True
 def main():
     # Showcase GMR and KMP on the single point task dataset
     # Load the demonstrations
-    datasets = load_datasets('demonstrations/single_point_task')
+    datasets = load_datasets('demonstrations/single_point_task', regex=r'tdataset(\d{2})\.npy')
     # Prepare the data for GMM/GMR
     H = len(datasets)  # Number of demonstrations
     N = len(datasets[0]) // subsample  # Length of each demonstration
@@ -90,7 +90,7 @@ def main():
     # KMP on the position
     if pos:
         kmp_dt = 0.01
-        x_kmp = kmp_dt*np.arange(1, demo_dura/kmp_dt + 1).reshape(1, -1)
+        x_kmp = np.arange(kmp_dt, demo_dura, kmp_dt).reshape(1, -1)
         kmp = KMP(l=0.5, alpha=40, sigma_f=1, verbose=True)
         kmp.fit(x_gmr, mu_pos, sigma_pos)
         mu_pos_kmp, sigma_pos_kmp = kmp.predict(x_kmp) 
@@ -98,7 +98,7 @@ def main():
         np.save(join(ROOT, 'trained_models/sigma_pos_kmp.npy'), sigma_pos_kmp)
     # KMP on the orientation
     if rot:
-        kmp_dt = 0.1
+        kmp_dt = 0.01
         x_kmp = np.arange(kmp_dt, demo_dura, kmp_dt).reshape(1, -1)
         kmp = KMP(l=0.5, alpha=30, sigma_f=2, verbose=True)
         kmp.fit(x_gmr, mu_rot, sigma_rot)
@@ -106,12 +106,13 @@ def main():
         np.save(join(ROOT, 'trained_models/mu_rot_kmp.npy'), mu_rot_kmp)
         np.save(join(ROOT, 'trained_models/sigma_rot_kmp.npy'), sigma_rot_kmp)
         quats_kmp = np.vstack((mu_rot_kmp[:3,:],np.zeros_like(mu_rot_kmp[0,:])))
-        for i in range(mu_rot.shape[1]):
+        for i in range(mu_rot_kmp.shape[1]):
             quats_kmp[:, i] = (Quaternion.exp(mu_rot_kmp[:3, i])*qa).as_array()
+        np.save(join(ROOT, 'trained_models/mu_rot_kmp_quats.npy'), quats_kmp)
     # KMP on the force
     if force_:
         kmp_dt = 0.01
-        x_kmp = kmp_dt*np.arange(1, demo_dura/kmp_dt + 1).reshape(1, -1)
+        x_kmp = np.arange(kmp_dt, demo_dura, kmp_dt).reshape(1, -1)
         kmp = KMP(l=0.5, alpha=40, sigma_f=1, verbose=True)
         kmp.fit(x_gmr, mu_force, sigma_force)
         mu_force_kmp, sigma_force_kmp = kmp.predict(x_kmp)
@@ -122,21 +123,19 @@ def main():
     for dataset in datasets:
         plot_demo(ax, dataset[::subsample], demo_dura)
     t_gmr = np.arange(gmm_dt, demo_dura, gmm_dt)
-    t_kmp = np.arange(kmp_dt, demo_dura+kmp_dt, kmp_dt)
+    t_kmp = np.arange(kmp_dt, demo_dura, kmp_dt)
     for i in range(3):
         if pos:
             ax[0, i].errorbar(x=t_gmr, y=mu_pos[i, :], yerr=np.sqrt(sigma_pos[i,i,:]), color='red', alpha=0.35)
             ax[0, i].errorbar(x=t_kmp, y=mu_pos_kmp[i, :], yerr=np.sqrt(sigma_pos_kmp[i,i,:]), color='green', alpha=0.25)
-        if rot:
-            """
-            # Plot Euclidean quaternions
+        if rot:            
+            """# Plot Euclidean quaternions
             ax[1, i].errorbar(x=t_gmr, y=mu_rot[i, :], yerr=np.sqrt(sigma_rot[i,i,:]), color='red', alpha=0.35)
             ax[1, i].errorbar(x=t_kmp, y=mu_rot_kmp[i, :], yerr=np.sqrt(sigma_rot_kmp[i,i,:]), color='green', alpha=0.1)
             ax[1, i].plot(t_gmr, mu_rot[i, :], color='green')
             ax[1, i].plot(t_kmp, mu_rot_kmp[i, :], color='green')"""
-            t = np.arange(0.1, demo_dura, 0.1)
             ax[1, i].plot(t_gmr, quats[i+1, :], color='red')
-            ax[1, i].plot(t, quats_kmp[i+1, :], color='green')
+            ax[1, i].plot(t_kmp, quats_kmp[i+1, :], color='green')
         if force_:
             ax[2, i].errorbar(x=t_gmr, y=mu_force[i, :], yerr=np.sqrt(sigma_force[i,i,:]), color='red', alpha=0.35)
             ax[2, i].errorbar(x=t_kmp, y=mu_force_kmp[i, :], yerr=np.sqrt(sigma_force_kmp[i,i,:]), color='green', alpha=0.25)
@@ -154,7 +153,10 @@ def plot_demo(ax, demonstration, dura, linestyle='solid', label=''):
     z = [p.z for p in demonstration]
     qx = [p.rot.as_array()[1] for p in demonstration]    
     qy = [p.rot.as_array()[2] for p in demonstration]    
-    qz = [p.rot.as_array()[3] for p in demonstration]    
+    qz = [p.rot.as_array()[3] for p in demonstration]  
+    """qx = [p.rot_eucl[0] for p in demonstration]    
+    qy = [p.rot_eucl[1] for p in demonstration]    
+    qz = [p.rot_eucl[2] for p in demonstration]    """
     fx = [p.fx for p in demonstration]    
     fy = [p.fy for p in demonstration]    
     fz = [p.fz for p in demonstration]

samples/reproduction.py
@@ -3,29 +3,128 @@ import rtde_receive
 import numpy as np
 import time
 from control import lqr
-from os.path import join, dirname, abspath
+from os.path import join, dirname, abspath, exists
 
 ROOT = dirname(dirname(abspath(__file__)))
 
-rtde_c = rtde_control.RTDEControlInterface("172.17.0.2")
-#rtde_r = rtde_receive.RTDEReceiveInterface("172.17.0.2")
+def symmetrize(matrices):
+    for i in range(matrices.shape[-1]):
+        matrix = matrices[:,:,i]
+        matrices[:,:,i] = (matrix + matrix.T) / 2
+    return matrices
+
+def to_axis_angle(rotations):
+    axis_angle = np.zeros((3, rotations.shape[-1]))
+    for i in range(rotations.shape[-1]):    
+        quaternion = np.array(rotations[:,i])
+        # Extract the angle of rotation
+        angle = 2*np.arccos(quaternion[0])  # Angle in radians
+        # Avoid division by zero for small angles
+        if np.abs(angle) > 1e-8:
+            # Extract the axis of rotation
+            axis = quaternion[1:] / np.sin(angle/2)
+            axis_angle[:,i] = (angle)*axis
+    return axis_angle
+
+rtde_c = rtde_control.RTDEControlInterface("192.168.1.11")
+rtde_r = rtde_receive.RTDEReceiveInterface("192.168.1.11")
 
 # Pull the KMP trajectories
 pos = np.load(join(ROOT, 'trained_models/mu_pos_kmp.npy'))
-rot = np.load(join(ROOT, 'trained_models/mu_rot_kmp.npy'))
-
+if not exists(join(ROOT, 'trained_models/mu_rot_kmp_axis_angle.npy')):
+    rot = np.load(join(ROOT, 'trained_models/mu_rot_kmp_quats.npy'))
+    rot = to_axis_angle(rot)
+    np.save(join(ROOT, 'trained_models/mu_rot_kmp_axis_angle.npy'), rot)
+else:
+    rot = np.load(join(ROOT, 'trained_models/mu_rot_kmp_axis_angle.npy'))
+poses = np.vstack((pos[:3,:], rot))
 # Pull the KMP uncertainties
 pos_uncert = np.load(join(ROOT, 'trained_models/sigma_pos_kmp.npy'))
+pos_uncert = symmetrize(pos_uncert)
 rot_uncert = np.load(join(ROOT, 'trained_models/sigma_rot_kmp.npy'))
-
+rot_uncert = symmetrize(rot_uncert)
 # Precompute precisions
+if not exists(join(ROOT, 'trained_models/sigma_pos_kmp_inv.npy')):
+    pos_uncert_inv = np.zeros_like(pos_uncert)
+    for i in range(pos_uncert.shape[2]):
+        pos_uncert_inv[:,:,i] = np.matrix.round(np.linalg.inv(pos_uncert[:,:,i]), 2)
+    np.save(join(ROOT, 'trained_models/sigma_pos_kmp_inv.npy'), pos_uncert_inv)
+else:
+    pos_uncert_inv = np.load(join(ROOT, 'trained_models/sigma_pos_kmp_inv.npy'))
+if not exists(join(ROOT, 'trained_models/sigma_rot_kmp_inv.npy')):
+    rot_uncert_inv = np.zeros_like(rot_uncert)
+    for i in range(rot_uncert.shape[2]):
+        rot_uncert_inv[:,:,i] = np.matrix.round(np.linalg.inv(rot_uncert[:,:,i]), 2)
+    np.save(join(ROOT, 'trained_models/sigma_rot_kmp_inv.npy'), rot_uncert_inv)
+else:
+    rot_uncert_inv = np.load(join(ROOT, 'trained_models/sigma_rot_kmp_inv.npy'))
+# Compute the gains for the position controller using LQR
+if not exists(join(ROOT, 'trained_models/KP_pos_gains.npy')):
+    A = np.block([[np.zeros((3,3)), np.eye(3)],[np.zeros((3,3)), np.zeros((3,3))]])
+    B = np.block([[np.zeros((3,3))],[np.eye(3)]])
+    gain_magnitude_penalty = 1000
+    R = np.eye(3)*gain_magnitude_penalty
+    # Gain computation
+    KP_pos = []
+    KD_pos = []
+    for i in range(pos_uncert_inv.shape[2]):
+        Q = pos_uncert_inv[:,:,i]
+        Q = (Q + Q.T) / 2
+        K, S, E = lqr(A, B, Q, R)
+        KP_pos.append(np.diag((K[0,0],K[1,1],K[2,2])))
+        KD_pos.append(np.diag((K[0,3],K[1,4],K[2,5])))
+    KP_pos = np.array(KP_pos)
+    KD_pos = np.array(KD_pos)
+    np.save(join(ROOT, 'trained_models/KP_pos_gains.npy'), KP_pos)
+    np.save(join(ROOT, 'trained_models/KD_pos_gains.npy'), KD_pos)
+else:
+    KP_pos = np.load(join(ROOT, 'trained_models/KP_pos_gains.npy'))
+    KD_pos = np.load(join(ROOT, 'trained_models/KD_pos_gains.npy'))
+# Compute the gains for the orientation controller using LQR
+if not exists(join(ROOT, 'trained_models/KP_rot_gains.npy')):
+    A = np.block([[np.zeros((3,3)), np.eye(3)],[np.zeros((3,3)), np.zeros((3,3))]])
+    B = np.block([[np.zeros((3,3))],[np.eye(3)]])
+    gain_magnitude_penalty = 1000
+    R = np.eye(3)*gain_magnitude_penalty
+    # Gain computation
+    KP_rot = []
+    KD_rot = []
+    for i in range(rot_uncert_inv.shape[2]):
+        Q = rot_uncert_inv[:,:,i]
+        Q = (Q + Q.T) / 2
+        K, S, E = lqr(A, B, Q, R)
+        KP_rot.append(np.diag((K[0,0],K[1,1],K[2,2])))
+        KD_rot.append(np.diag((K[0,3],K[1,4],K[2,5])))
+    KP_rot = np.array(KP_rot)
+    KD_rot = np.array(KD_rot)
+    np.save(join(ROOT, 'trained_models/KP_rot_gains.npy'), KP_rot)
+    np.save(join(ROOT, 'trained_models/KD_rot_gains.npy'), KD_rot)
+else:
+    KP_rot = np.load(join(ROOT, 'trained_models/KP_rot_gains.npy'))
+    KD_rot = np.load(join(ROOT, 'trained_models/KD_rot_gains.npy'))
 
-# Zero the FT sensor
-#rtde_c.zeroFtSensor()
+# Go home
+rtde_c.moveJ([0.0, -np.pi/2, 0.0, -np.pi/2, 0.0, 0.0])
 # Go to the start of the trajectory
-print([pos[0,0], pos[1,0], pos[2,0], rot[0,0], rot[1,0], rot[2,0]])
+pose = poses[:,0]
+rtde_c.moveJ_IK(pose=pose, speed = 0.5)
 # Execute the trajectory
-"""while True:
+for i in range(poses.shape[-1]):
     t_start = rtde_c.initPeriod()
+    # Get the state of the robot
+    current_pose = rtde_r.getActualTCPPose()
+    current_speed = rtde_r.getActualTCPSpeed()
+    # Compute the pose error
+    xf1 = (poses[:3,i] - current_pose[:3]).reshape(-1,1)
+    xf2 = (poses[3:,i] - current_pose[3:]).reshape(-1,1)
+    # Compute the velocity command
+    dxe1 = (KD_pos[i,:,:]@(KP_pos[i,:,:]@xf1 - np.array(current_speed[:3]).reshape(-1,1))).flatten()
+    dxe2 = (KD_rot[i,:,:]@(KP_rot[i,:,:]@xf2 - np.array(current_speed[3:]).reshape(-1,1))).flatten()
+    dxe = np.concatenate((dxe1, [0.0,0.0,0.0]))#dxe2))
+    # Send the commands
+    rtde_c.speedL(dxe)
     rtde_c.waitPeriod(t_start)
-rtde_c.stopScript()"""
+# Go home
+rtde_c.moveJ([0.0, -np.pi/2, 0.0, -np.pi/2, 0.0, 0.0])
+rtde_c.speedStop()
+rtde_c.stopScript()

src/dataset/dataset.py
@@ -9,8 +9,7 @@ from tslearn.metrics import soft_dtw_alignment
 
 
 ROOT = dirname(dirname(dirname(abspath(__file__))))
-
-
+UR5_base_position = np.array([1.808655,	0.885495,	1.448405])
 def create_dataset(demonstrations_path: str = '', demonstration_regex: str = r'') -> None:
     """Process a set of demonstration recordings into an usable dataset
 
@@ -28,7 +27,7 @@ def create_dataset(demonstrations_path: str = '', demonstration_regex: str = r''
     out = []
     for i, file in enumerate(files):
         t_cnt = 1
-        t_dt = 0.001
+        t_dt = 0.1
         with open(join(demonstrations_path, file)) as csv_file:
             reader = csv.DictReader(csv_file)
             for j, row in enumerate(reader):
@@ -83,7 +82,7 @@ def as_array(dataset):
     return np.vstack([point.as_array() for point in dataset])
 
 def from_array(array):
-   return [Point.from_array(row) for row in array.T]
+   return [Point.from_array(row) for row in array]
 
 def interpolate_datasets(datasets_path: str = ''):
     """Fill in the force-only samples with a linear interpolation between the previous and next full samples.
@@ -128,6 +127,43 @@ def interpolate_datasets(datasets_path: str = ''):
         np.save(join(ROOT, datasets_path, file), dataset)
 
 
+def to_base_frame(datasets_path: str = '') -> None:
+    """Transform the coordinates to the base frame of the robot
+
+    Parameters
+    ----------
+    datasets_path : str, default = ''
+        The path to the datasets, relative to ROOT.
+    """
+    datasets_path = join(ROOT, datasets_path)
+    regex = r'dataset(\d{2})\.npy'
+    files = [f for f in listdir(datasets_path) if re.match(regex, f) is not None]
+    files.sort()
+    # Rotation of the robot base frame wrt Motive frame, expressed in quaternion form
+    q0 = Quaternion.from_array([np.sqrt(2)/2, -np.sqrt(2)/2, 0, 0])
+    theta = -np.pi
+    R = np.array([[1, 0, 0],
+                  [0, np.cos(theta), -np.sin(theta)],
+                  [0, np.sin(theta), np.cos(theta)]])
+    for file in files:
+        dataset = np.load(join(datasets_path, file), allow_pickle=True)
+        new = as_array(dataset)
+        # TODO: this would probably look better with a T matrix instead of hardcoding the rototranslation
+        # Translation
+        new[:, 1:4] -= UR5_base_position
+        # Rotation
+        new[:, [2, 3]] = new[:, [3, 2]]
+        new[:, 2] *= -1
+        # Quaternions
+        for i in range(new.shape[0]):
+            old_quat = Quaternion.from_array(new[i, 4:8])
+            #new_quat = q0 * old_quat * ~q0
+            old_quat_R = old_quat.as_rotation_matrix()
+            new_quat_R = R@old_quat_R.T
+            new_quat = Quaternion.from_rotation_matrix(new_quat_R)
+            new[i, 4:8] = new_quat.as_array()
+        np.save(join(ROOT, datasets_path, "t" + file), from_array(new))
+
 def compute_alignment_path(cost_matrix):
     """Computes the warping path from a cost matrix.
 
@@ -157,9 +193,11 @@ def align_datasets(datasets_path: str = ''):
     for i, dataset in enumerate(datasets):
         if i > 0:
             cost_matrix, _ = soft_dtw_alignment(as_array(dataset)[:, 1:8], reference, gamma=2.5)
+            for j, point in enumerate(dataset):
+                point.timestamp = (j + 1) * 0.1
             np.save(join(ROOT, datasets_path, f'dataset{i:02d}.npy'), dataset[compute_alignment_path(cost_matrix)])
 
-def load_datasets(datasets_path: str='', regex = r'dataset(\d{2})\.npy') -> np.ndarray:
+def load_datasets(datasets_path: str='', regex = r'tdataset(\d{2})\.npy') -> np.ndarray:
     """Load all datasets in the given folder.
 
     Parameters

src/datatypes/Quaternion.py
@@ -39,6 +39,21 @@ class Quaternion:
         v = np.abs(np.cos(angle/2))
         u = axis*np.sin(angle/2)
         return cls(v, u)
+    
+    def as_rotation_vector(self) -> ArrayLike:
+        """Converts the quaternion to a rotation vector.
+
+        Returns
+        -------
+        array-like of shape (3,)
+            The rotation vector.
+        """
+        angle = 2*np.arccos(self.v)
+        if np.abs(angle) > 1e-8:
+            axis = self.u / np.sin(angle/2)
+        else:
+            axis = np.zeros(3)
+        return angle*axis
 
     @classmethod
     def from_array(cls, array: ArrayLike) -> 'Quaternion':
@@ -55,6 +70,64 @@ class Quaternion:
             The corresponding quaternion
         """
         return cls(array[0], array[1:])
+    
+    def as_array(self) -> ArrayLike:
+        """Returns the quaternion's components as an array, with the scalar component first.
+
+        Returns
+        -------
+        array-like of shape (4,)
+            The array representing the quaternion.
+        """
+        return np.concatenate(([self.v], self.u))
+
+    @classmethod
+    def from_rotation_matrix(cls, R: ArrayLike) -> 'Quaternion':
+        """Creates a quaternion from a rotation matrix.
+
+        Parameters
+        ----------
+        array : array-like of shape (3,3)
+            The rotation matrix.
+
+        Returns
+        -------
+        quaternion
+            The corresponding quaternion
+        """
+        trace = R[0,0] + R[1,1] + R[2,2]
+        if trace > 0:
+            S = np.sqrt(trace + 1) * 2
+            v = 0.25 * S
+            u = np.array([(R[2,1] - R[1,2]) / S, (R[0,2] - R[2,0]) / S, (R[1,0] - R[0,1]) / S])
+        elif R[0,0] > R[1,1] and R[0,0] > R[2,2]:
+            S = np.sqrt(1 + R[0,0] - R[1,1] - R[2,2]) * 2
+            v =(R[2,1] - R[1,2]) / S
+            u = np.array([0.25 * S, (R[0,1] - R[1,0]) / S, (R[0,2] - R[2,0]) / S])
+        elif R[1,1] > R[2,2]:
+            S = np.sqrt(1 + R[1,1] - R[0,0] - R[2,2]) * 2
+            v = (R[0,2] - R[2,0]) / S
+            u = np.array([(R[0,1] - R[1,0]) / S, 0.25 * S, (R[1,2] - R[2,1]) / S])
+        else:
+            S = np.sqrt(1 + R[2,2] - R[0,0] - R[1,1]) * 2
+            v = (R[1,0] - R[0,1]) / S
+            u = np.array([(R[0,2] - R[2,0]) / S, (R[1,2] - R[2,1]) / S, 0.25 * S])
+        return cls(v, u)
+
+    def as_rotation_matrix(self) -> np.array:
+        """Return the rotation matrix representation of self.
+        
+        Returns
+        -------
+        ArrayLike
+            The rotation matrix corresponding to self.
+        """
+        w = self.v
+        x, y, z = self.u
+        out = np.array([[1 - 2*y**2 - 2*z**2, 2*x*y - 2*w*z, 2*x*z + 2*w*y],
+                        [2*x*y + 2*w*z, 1 - 2*x**2 - 2*z**2, 2*y*z - 2*w*x],
+                        [2*x*z - 2*w*y, 2*y*z + 2*w*x, 1 - 2*x**2 - 2*y**2 ]])
+        return out
 
     @classmethod
     def exp(cls, w: ArrayLike) -> 'Quaternion':
@@ -103,16 +176,6 @@ class Quaternion:
             The vector of absolute values of self's components.
         """
         return np.concatenate(([np.abs(self.v)], np.abs(self.u)))
-
-    def as_array(self) -> ArrayLike:
-        """Returns the quaternion's components as an array, with the scalar component first.
-
-        Returns
-        -------
-        array-like of shape (4,)
-            The array representing the quaternion.
-        """
-        return np.concatenate(([self.v], self.u))
 
     def normalize(self) -> 'Quaternion':
         """Normalizes self.